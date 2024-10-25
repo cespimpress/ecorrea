da4fbb7fbf912fe5abc21cbddd9db4182f2d447e
cespimpress <43565543+cespimpress@users.noreply.github.com>
2024-10-25 20:50:57 +0200

    Add files via upload

--- files/click-to-play-video/index.php
+++ files/click-to-play-video/index.php
@@ -1,5 +1,5 @@
 <?php
 header("Status: 301 Moved Permanently");
-header("Location: https://www.youtube.com/embed/pYAweqHACBo");
+header("Location: https://www.youtube.com/embed/S4BDuNc1fO8");
 exit;
 ?>
