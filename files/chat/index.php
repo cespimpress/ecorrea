6a7e8dcb0177a3ebd5aee3e00357c717c2af341d
cespimpress <43565543+cespimpress@users.noreply.github.com>
2024-10-25 20:26:59 +0200

    Add files via upload

---
+++ files/chat/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Status: 301 Moved Permanently");
+header("Location: https://api.whatsapp.com/send/?phone=34624630136&text=Buen+d%C3%ADa%2C+me+comunico+desde+su+IdCard%2C+deseo+hacerle+una+consulta&app_absent=0");
+exit;
+?>
